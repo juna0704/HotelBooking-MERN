ff58b4385fd787de592b11670e65fc4d30610347
jak0704 <junaidalikhan0704@gmail.com>
2025-03-05 23:40:23 +0530

    added layout in app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,20 @@
-function App() {
-	return;
-}
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+	Navigate,
+} from 'react-router-dom';
+import Layout from './layouts/Layout';
+
+const App = () => {
+	return (
+		<Router>
+			<Routes>
+				<Route path='/' element={<Layout></Layout>} />
+				<Route path='*' element={<Navigate to='/' />} />
+			</Routes>
+		</Router>
+	);
+};
 
 export default App;
