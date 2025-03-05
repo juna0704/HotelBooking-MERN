d7d5396c9ab0c0746e901e19b533030f21b58ad7
jak0704 <junaidalikhan0704@gmail.com>
2025-03-05 23:43:11 +0530

    added footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
-
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<div className='bg-blue-800 py-10'>
+			<div className='container mx-auto flex justify-between items-center'>
+				<span className='text-3xl text-white font-bold tracking-tight'>
+					MernHolidays.com
+				</span>
+				<span className='text-white font-bold tracking-tight flex gap-4'>
+					<p className='cursor-pointer'>Privacy Policy</p>
+					<p className='cursor-pointer'>Terms of Service</p>
+				</span>
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
