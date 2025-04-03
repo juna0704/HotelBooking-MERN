ee8227c30d5d881ef51aef938b6acb6d8349979c
junaidalikhan <junaidalikhan0704@gmail.com>
2025-04-03 23:16:40 +0530

    added index.ts routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,34 +1,47 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import mongoose from 'mongoose';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth";
+import cookieParser from "cookie-parser";
+import path from "path";
+import { v2 as cloudinary } from "cloudinary";
+import myHotelRoutes from "./routes/my-hotels";
+import hotelRoutes from "./routes/hotels";
+import bookingRoutes from "./routes/my-bookings";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
-if (!process.env.MONGODB_CONNECTION_STRING) {
-	console.error(
-		'MONGODB_CONNECTION_STRING is not defined in the environment variables'
-	);
-	process.exit(1);
-}
-
-mongoose
-	.connect(process.env.MONGODB_CONNECTION_STRING)
-	.then(() => console.log('Connected to MongoDB'))
-	.catch((error) => {
-		console.error('MongoDB connection error:', error);
-		process.exit(1);
-	});
-
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
-app.get('/api/test', async (req: Request, res: Response) => {
-	res.json({ message: 'hello from express endpoint' });
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/my-hotels", myHotelRoutes);
+app.use("/api/hotels", hotelRoutes);
+app.use("/api/my-bookings", bookingRoutes);
+
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
 });
 
 app.listen(7000, () => {
-	console.log('server is running on localhost:7000');
+  console.log("server running on localhost:7000");
 });
